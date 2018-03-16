ea49e2b7f4e8534d56ba8001d7ea125c68646b53
Daniel <danielcox16@hotmail.com>
2018-03-16 16:32:19 +0100

    [COLLECT]: Configure for Indonesian region

--- collect_cfg.py
+++ collect_cfg.py
@@ -9,12 +9,13 @@ cfg['month']   = 8                 # Month of interest
 
 # Define Region of Interest Coordinates
 ROI = {}
-ROI['xll']     = -97.75            # X Lower Left
-ROI['yll']     =  41.93            # Y Lower Left
-ROI['xur']     = -97.32            # X Upper Right
-ROI['yur']     =  42.19            # Y Upper Right
+ROI['xll']     = 103               # X Lower Left
+ROI['yll']     =   1               # Y Lower Left
+ROI['xur']     = 104               # X Upper Right
+ROI['yur']     =   2               # Y Upper Right
 cfg['ROI']     = ROI
 
 # Product URLs
 cfg['baseurl']  = 'http://www.vito-eodata.be/PDF/datapool/Free_Data/'
-cfg['products'] = ['PROBA-V_100m/S1_TOC_100_m_C1/', 'PROBA-V_300m/S1_TOC_-_300_m_C1/']
+# cfg['products'] = ['PROBA-V_100m/S1_TOC_100_m_C1/', 'PROBA-V_300m/S1_TOC_-_300_m_C1/']
+cfg['products'] = ['PROBA-V_300m/L2A_-_300_m_C1/', 'PROBA-V_100m/L2A_-_100_m_C1/']
