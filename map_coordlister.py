d0344e6bef3c0cd272e2575f26aeb9bedb69a52f
Daniel <danielcox16@hotmail.com>
2018-03-26 11:28:08 +0200

    [MAP]: Add exception for corrupt files
    In order to deal with corrupt files, I had to add an exception. This made it easy to remove the corrupt files.

--- map_coordlister.py
+++ map_coordlister.py
@@ -5,6 +5,7 @@ import h5py
 
 # Return a list of unique coordinates contained in the database
 def coordlister(paths, mapcfg):
+    
     # Construct HDF5 database iterator
     pattern = os.path.join(paths['data'], '**/*.[Hh][Dd][Ff]5')
     it = glob.iglob(pattern, recursive = True)
@@ -13,39 +14,48 @@ def coordlister(paths, mapcfg):
     uniROIdata = []
     allROIdata = []
     
+    nerr = 0
+    
     # Iterate over all HDF5 files
     for filepath in it:
-        with h5py.File(filepath, 'r') as f:
-            
-            # Determine product by matching product list items against filepath string
-            for product in mapcfg['products']:
-                if product in filepath:
-                    break
+        try:
+            with h5py.File(filepath, 'r') as f:
                 
-            else:
-                # Product not found
-                product = ''
-            
-            iprod = mapcfg['products'].index(product)
-            
-            # Read min/max longitude/lattitude
-            lon1 = f['lon'][0]
-            lon2 = f['lon'][-1]
-            lat1 = f['lat'][0]
-            lat2 = f['lat'][-1]
-            
-            # Construct ROI info
-            coord = (lon1, lon2, lat1, lat2)
-            ROI = {
-                'coord':    coord,
-                'iprod':    iprod,
-                'filepath': filepath
-            }
-            
-            allROIdata.append(ROI)
-            
-            # Add ROI data if unique
-            if not(ROI in uniROIdata):
-                uniROIdata.append(ROI)        # Add to ROIdata
+                # Determine product by matching product list items against filepath string
+                for product in mapcfg['products']:
+                    if product in filepath:
+                        break
+                    
+                else:
+                    # Product not found
+                    product = ''
+                
+                iprod = mapcfg['products'].index(product)
+                
+                # Read min/max longitude/lattitude
+                lon1 = f['lon'][0]
+                lon2 = f['lon'][-1]
+                lat1 = f['lat'][0]
+                lat2 = f['lat'][-1]
+                
+                # Construct ROI info
+                coord = (lon1, lon2, lat1, lat2)
+                ROI = {
+                    'coord':    coord,
+                    'iprod':    iprod,
+                    'filepath': filepath
+                }
+                
+                # Add ROI data to full list
+                allROIdata.append(ROI)
+                
+                # If unique, add ROI data to unique list
+                if not(ROI in uniROIdata):
+                    uniROIdata.append(ROI)        # Add to ROIdata
+
+        # Check if any file loading errors occur
+        except OSError:
+            nerr += 1
+            print('Error loading file! (total errors: {})\n'.format(nerr))
         
     return allROIdata, uniROIdata
