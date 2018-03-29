11836a5f6edd149d37dc87d6ce958cc99bd93588
Daniel <danielcox16@hotmail.com>
2018-03-29 17:00:04 +0200

    [MAP]: Use sizestr for filesize string

--- map.py
+++ map.py
@@ -2,11 +2,11 @@
 import os
 import numpy as np
 
+from custom_functions import *
 from paths import *
 from map_coordlister import *
 from map_writer import *
 from map_genjs import *
-
 from map_cfg import *
 
 allROIdata, uniROIdata = coordlister(paths, mapcfg) # List all unique ROI
@@ -16,5 +16,5 @@ mapper(uniROIdata, paths, mapcfg)                   # Mark regions in layers
 # Write ROI data to file
 filename = mapcfg['ROIfilename']                    # File name
 np.save(filename, allROIdata)                       # Save file
-filesize = int(os.path.getsize(filename) / 1000)    # File size (KB) rounded
-print("Saved {} ROI entries to file '{}' ({} KB).\n".format(len(allROIdata), filename, filesize))
+filesize = sizestr(os.path.getsize(filename), sep=' ')    # File size (KB) rounded
+print("Saved {} ROI entries to file '{}' ({}).\n".format(len(allROIdata), filename, filesize))
