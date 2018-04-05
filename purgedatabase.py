8d5055239c636c299dcbaeea8e0d8e4a784e8353
Daniel <danielcox16@hotmail.com>
2018-04-05 11:38:01 +0200

    [PURGE]: Add verbose -v option
    Paths of removed files are no longer reported unless verbose option is used: "purgedatabase.py -v"

--- purgedatabase.py
+++ purgedatabase.py
@@ -12,22 +12,28 @@ from progress.bar import ShadyBar
 from paths import *
 
 
+# Iterate recursively over HDF5 children and check for keys
 def checkitems(f):
-    # Iterate recursively over HDF5 children
     if hasattr(f, 'keys'):
         for key in f.keys():
             checkitems(f[key])
         
+    
+# Parse optional argument '-v' for verbose output
+verbose = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-v':
+        verbose = True
+
     
 # Construct HDF5 database list
 pattern = os.path.join(paths['data'], '**/*.[Hh][Dd][Ff]5')
 filepaths = glob.glob(pattern, recursive = True)
 
 # Initialise progress bar and error counter
+print('')
 bar = ShadyBar('Checking database files...', max=len(filepaths), width=25)
 nerr = 0
-
-print('\n')
 
 # Iterate over all HDF5 files
 for filepath in filepaths:
@@ -40,9 +46,11 @@ for filepath in filepaths:
     # Check if any file loading errors occur
     except (KeyError, RuntimeError, OSError):
         nerr += 1                   # Count file errors
-        print('\nCould not load file:\n' + filepath)
-        print('Removing file.\n')
         os.remove(filepath)         # Delete file!
+        
+        if verbose:
+            print('\nCould not load file:\n' + filepath)
+            print('Removing file.\n')
         
     bar.next()                      # Show progress bar
 
