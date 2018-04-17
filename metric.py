53ca0e425b33d45ad8f2f3427c66f7a6f1560bf8
Daniel <danielcox16@hotmail.com>
2018-04-17 15:53:24 +0200

    [METRIC]: Add SSIM and VIFP

--- metric.py
+++ metric.py
@@ -4,15 +4,29 @@
 import os, glob
 from libtiff import TIFF
 
+from videoquality.ssim import ssim_exact
+from videoquality.vifp import vifp_mscale
+
 from psnr import psnr
 from paths import *
 
+# Iterate over subfolders in SR results folder
 for root, dirs, files in os.walk(paths['srresults']):
     pattern = os.path.join(root, '*.tif')
     files = glob.glob(pattern)
     
+    # Compute metrics if image pair is found
     if len(files) >= 2:
+        
+        # Open the files
         img1 = TIFF.open(files[0], mode='r').read_image()
         img2 = TIFF.open(files[1], mode='r').read_image()
         
-        print('PSNR = {:.2f}'.format(psnr(img1, img2)))
+        # Compute metrics
+        MAX = 2**16-1
+        impsnr = psnr(img1, img2)
+        imssim = ssim_exact(img1/MAX, img2/MAX)
+        imvifp = vifp_mscale(img1, img2)
+        
+        # Show results
+        print('PSNR = {:.2f}, SSIM = {:.4f}, VIFP = {:.3f}'.format(impsnr, imssim, imvifp))
