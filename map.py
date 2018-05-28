555954739f306d01c9e74d4e2f198c45336589bb
Daniel <danielcox16@hotmail.com>
2018-05-28 13:43:52 +0200

    [MAP]: Linting

--- map.py
+++ map.py
@@ -5,16 +5,17 @@ Create earth map of database contents.
 Writes layer images to /map/img folder. Location of map file: /map/map.html.
 """
 
-import os
+from os.path import getsize
 import numpy as np
 
-from custom_functions import *
-from paths import *
-from map_coordlister import *
-from map_writer import *
-from map_genjs import *
-from map_cfg import *
-from couple_coords import *
+from custom_functions import sizestr
+from paths import paths
+from map_coordlister import coordlister
+from map_writer import mapper
+from map_genjs import genjs
+from map_cfg import mapcfg
+from couple_coords import coords
+
 
 print('\n=== MAP ===')
 
@@ -23,7 +24,8 @@ genjs(mapcfg)                                        # Generate javascript
 mapper(uniROIdata, paths, mapcfg, coords)            # Mark regions in layers
 
 # Write ROI data to file
-filename = mapcfg['ROIfilename']                    # File name
-np.save(filename, allROIdata)                       # Save file
-filesize = sizestr(os.path.getsize(filename), sep=' ')    # File size (KB) rounded
-print("Saved {} ROI entries to file '{}' ({}).\n".format(len(allROIdata), filename, filesize))
+filename = mapcfg['ROIfilename']                     # File name
+np.save(filename, allROIdata)                        # Save file
+filesize = sizestr(getsize(filename), sep=' ')       # File size (KB) rounded
+print("Saved {} ROI entries to file '{}' ({}).\n"
+      .format(len(allROIdata), filename, filesize))
