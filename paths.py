158febeb81b5d7a75443a9fc85da5d80f787edac
Daniel <danielcox16@hotmail.com>
2018-05-28 11:53:57 +0200

    [MAIN]: Lint paths.py

--- paths.py
+++ paths.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-import os
+"""
+Holds locations of dedicated folder paths.
+
+Contains location of: data folder, wget executable, tiles folder,
+kelvinspath folder, srresults folder.
+wget exe can be specified per platform.
+"""
+from os import path, name
 
 # Initialise paths dictionary
 paths = {}
@@ -8,26 +15,29 @@ paths = {}
 # N.B. Relative to proba-v-data-collector.py
 datarelpath = '/../data'        # Points to data folder
 wgetrelpath = '/../wget/wget'   # Points to wget executable (Windows only)
-tilespath   = '/tiles'          # Points to tiles folder
+tilespath = '/tiles'            # Points to tiles folder
 kelvinspath = '/kelvinsset'     # points to kelvinsset folder
-srresults   = '/sr/results'     # Points to SR results folder
-# Note: As wget is usually installed by default on Linux distros, it is assumed this command is present. paths.py deals with platform specific cases such as these.
+srresults = '/sr/results'       # Points to SR results folder
 
-#=============================================================#
+# Note: As wget is installed by default on many Linux distros,
+#       it is assumed this command is present. paths.py deals with platform
+#       specific cases such as these.
+
+# ============================================================= #
 # Construct common paths
-repopath = os.path.dirname(__file__)
-paths['data'] = os.path.realpath(repopath + datarelpath)
-paths['tiles'] = os.path.realpath(repopath + tilespath)
-paths['kelvinsset'] = os.path.realpath(repopath + kelvinspath)
-paths['srresults'] = os.path.realpath(repopath + srresults)
+repopath = path.dirname(__file__)
+paths['data'] = path.realpath(repopath + datarelpath)
+paths['tiles'] = path.realpath(repopath + tilespath)
+paths['kelvinsset'] = path.realpath(repopath + kelvinspath)
+paths['srresults'] = path.realpath(repopath + srresults)
 
 # Construct specific paths
-if os.name == "nt":
+if name == "nt":
     # Construct absolute paths from current file path
-    wgetpath = os.path.realpath(repopath + wgetrelpath) # Points to wget executable in Windows
+    wgetpath = path.realpath(repopath + wgetrelpath)  # wget exe in Windows
     paths['wget'] = wgetpath
-    
-elif os.name == "posix":
+
+elif name == "posix":
     # Define relative paths (N.B. Relative to proba-v-data-collector.py)
     wgetpath = 'wget'                       # Points to wget command in Linux
     paths['wget'] = wgetpath
